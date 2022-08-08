e723dacdf84871630c71abc64baa10b4f92cf514
Tirth Patel <tirthgpatel.27@gmail.com>
2022-08-09 00:30:00 +0530

    files count output added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const path = require('path')
+const glob = require('glob')
 
 try {
 	// `who-to-greet` input defined in action metadata file
@@ -7,9 +9,12 @@ try {
 	console.log(`Hello ${nameToGreet}!`)
 	const time = new Date().toTimeString()
 	core.setOutput('time', time)
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2)
-	console.log(`The event payload: ${payload}`)
+	const workSpace = github.context.workspace
+	core.setOutput('workspace', workSpace)
+
+	// access the github repository and count JS,TS,JSX,TSX files
+	const files = glob.sync(path.join(workSpace, '**/*.+(js|jsx|ts|tsx)'))
+	core.setOutput('files', files.length)
 } catch (error) {
 	core.setFailed(error.message)
 }
