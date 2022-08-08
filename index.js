6b3ef23c2f75d3d60dc1adfd0528475545cb42d9
Tirth Patel <tirthgpatel.27@gmail.com>
2022-08-09 01:15:32 +0530

    Output variable updated

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const path = require('path')
 const glob = require('glob')
 
-try {
-	const time = new Date().toTimeString()
-	core.setOutput('time', time)
-	const githubContext = github.context
-	console.log(`Github context: ${JSON.stringify(githubContext)}`)
-	const workSpace = github.context.workflow
-	core.setOutput('workspace', workSpace)
+const isDevelopment = process.env.NODE_ENV === 'development'
+const developmentCwd = './.github/workflows/'
 
-	const rootPath = workSpace ?? './'
+try {
+	const currentPath = isDevelopment ? developmentCwd : process.cwd()
+	// go to 2 levels up for repo root
+	const rootPath = path.join(currentPath, '../..')
 
 	// access the github repository and count JS,TS,JSX,TSX files and exclude node_modules
 	const files = glob.sync(path.join(rootPath, '**/*.{js,ts,jsx,tsx}'), {
 		ignore: 'node_modules/**',
 	})
+	console.log(`Found ${files.length} files`)
 	core.setOutput('files', files.length)
 } catch (error) {
 	core.setFailed(error.message)
