a3e6c6e57aa27947213becb13dd228429f483132
Tirth Patel <tirthgpatel.27@gmail.com>
2022-08-09 00:45:16 +0530

    Action file changes, node_modules ignored

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const path = require('path')
 const glob = require('glob')
 
 try {
-	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = core.getInput('who-to-greet')
-	console.log(`Hello ${nameToGreet}!`)
 	const time = new Date().toTimeString()
 	core.setOutput('time', time)
 	const workSpace = github.context.workspace
 	core.setOutput('workspace', workSpace)
 
-	// access the github repository and count JS,TS,JSX,TSX files
-	const files = glob.sync(path.join(workSpace, '**/*.+(js|jsx|ts|tsx)'))
+	// access the github repository and count JS,TS,JSX,TSX files and exclude node_modules
+	const files = glob.sync(path.join(workSpace, '**/*.{js,ts,jsx,tsx}'), {
+		ignore: 'node_modules/**',
+	})
 	core.setOutput('files', files.length)
 } catch (error) {
 	core.setFailed(error.message)
